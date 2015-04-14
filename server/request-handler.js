aa36f004ffccc6276ec3544f0041fecd77efc56e
dgrundfest <dave4@grundfest.com>
2015-04-13 19:38:49 -0700

    completed tests for chatterbox-server

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,38 +12,56 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
+  var messages = {
+    results: []
+  };
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log("Serving request type " + request.method + " for url " + request.url);
+exports.requestHandler = function(request, response) {
+
+
+
+ // console.log(request.method, request.url);
+
+
+  // Request and Response come from node's http module.
+    //
+    // They include information about both the incoming request, such as
+    // headers and URL, and about the outgoing response, such as its status
+    // and content.
+    //
+    // Documentation for both request and response can be found in the HTTP section at
+    // http://nodejs.org/documentation/api/
+
+    // Do some basic logging.
+    //
+    // Adding more logging to your server can be an easy way to get passive
+    // debugging help, but you should always be careful about leaving stray
+    // console.logs in your code.
+ // console.log("Serving request type " + request.method + " for url " + request.url);
 
   // The outgoing status.
-  var statusCode = 200;
+  var statusCode = 404;
 
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
+  headers['Content-Type'] = "text/plain";
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
+
+
+  if(request.method === 'GET' && request.url === '/classes/messages') {
+    statusCode = 200;
+    response.writeHead(statusCode, headers);
+    response.write(JSON.stringify(messages));
+    console.log('Get request',JSON.stringify(messages));
+    response.end();
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,9 +70,32 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end("Hello, World!");
-};
 
+  }else if(request.method === 'POST' && request.url === '/classes/messages') {
+    statusCode = 201;
+
+    var requestString = '';
+
+    request.on('data', function(data) {
+      requestString += data;
+    });
+
+    // on end of receiving data from request, push object
+    request.on('end', function() {
+      var message = JSON.parse(requestString);
+      messages.results.push(message);
+      response.writeHead(statusCode, headers);
+      response.end(requestString);
+      console.log('Post request',messages);
+    });
+
+  }else{
+    statusCode = 404;
+    response.writeHead(statusCode, headers);
+    response.end();
+  }
+
+};
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
 // are on different domains, for instance, your chat client.
--- server/spec/LiveServerIntegrationSpec.js
+++ server/spec/LiveServerIntegrationSpec.js
@@ -58,7 +58,13 @@ describe('server', function() {
 
     request(requestParams, function(error, response, body) {
       // Now if we request the log, that message we posted should be there:
+      //
+      //
+
+      //
       request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
+        // console.log('body',body);
+        // console.log('status',response.statusCode);
           var messages = JSON.parse(body).results;
           expect(messages[0].username).to.equal('Jono');
           expect(messages[0].message).to.equal('Do my bidding!');
@@ -75,4 +81,4 @@ describe('server', function() {
   });
 
 
-});
+ });
